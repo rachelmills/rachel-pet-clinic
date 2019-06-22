e65952436644559cbc8749223b04843d0da3821c
rachel.mills <rachel.h.mills@gmail.com>
2019-06-22 18:09:44 +1000

    First commit of petclinic initialised with Spring Initializr.

---
+++ src/main/java/com/mills/rachelpetclinic/RachelPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.mills.rachelpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachelPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachelPetClinicApplication.class, args);
+	}
+
+}
