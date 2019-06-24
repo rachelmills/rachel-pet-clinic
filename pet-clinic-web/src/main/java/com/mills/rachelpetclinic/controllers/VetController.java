80299e677475d8538f9c1fedd407acaf169985ba
rachel.mills <rachel.h.mills@gmail.com>
2019-06-24 20:37:02 +1000

    Added vet controller.

---
+++ pet-clinic-web/src/main/java/com/mills/rachelpetclinic/controllers/VetController.java
@@ -0,0 +1,16 @@
+package com.mills.rachelpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by rachelmills on 24/6/19.
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
