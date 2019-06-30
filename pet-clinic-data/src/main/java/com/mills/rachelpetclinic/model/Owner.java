1c03a4a3bd315433e465a9c675fd827fc631bec6
rachel.mills <rachel.h.mills@gmail.com>
2019-06-30 16:15:51 +1000

    Create Visit entity.

--- pet-clinic-data/src/main/java/com/mills/rachelpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.mills.rachelpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by rachelmills on 23/6/19.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
