a223cd8ab0005049a8bbc6b40cbc5e4b12f6156b
rachel.mills <rachel.h.mills@gmail.com>
2019-06-30 16:26:25 +1000

    Add PetTypeService and PetTypeMapService.

---
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.mills.rachelpetclinic.services;
+
+import com.mills.rachelpetclinic.model.PetType;
+
+/**
+ * Created by rachelmills on 30/6/19.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,39 @@
+package com.mills.rachelpetclinic.services.map;
+
+import com.mills.rachelpetclinic.model.PetType;
+import com.mills.rachelpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by rachelmills on 30/6/19.
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
