69b981653f80b4afa34436dd21c80f51a152acdf
rachel.mills <rachel.h.mills@gmail.com>
2019-06-30 16:17:52 +1000

    Create vet speciality class and add to vet.

---
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.mills.rachelpetclinic.model;
+
+/**
+ * Created by rachelmills on 30/6/19.
+ */
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
