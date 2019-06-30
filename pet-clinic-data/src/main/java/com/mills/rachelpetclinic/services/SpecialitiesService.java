83d2a9f4379801e7a50bafb46487ca9bf8c5b311
rachel.mills <rachel.h.mills@gmail.com>
2019-06-30 17:09:17 +1000

    Add specialitiesmapservice.

---
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.mills.rachelpetclinic.services;
+
+import com.mills.rachelpetclinic.model.Speciality;
+
+/**
+ * Created by rachelmills on 30/6/19.
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,38 @@
+package com.mills.rachelpetclinic.services.map;
+
+import com.mills.rachelpetclinic.model.Speciality;
+import com.mills.rachelpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by rachelmills on 30/6/19.
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
