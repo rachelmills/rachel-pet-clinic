1cd8056eb3ac849acf59390679f68e24b6295bf0
rachel.mills <rachel.h.mills@gmail.com>
2019-07-07 10:51:15 +1000

    Implement BaseEntity.

--- pet-clinic-data/src/main/java/com/mills/rachelpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mills/rachelpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.mills.rachelpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by rachelmills on 23/6/19.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
